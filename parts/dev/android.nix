fa4a56aeadc7a54756f39e9e02850db4e9757176
chervince <chervince@users.noreply.github.com>
2026-03-23 15:20:58 +1100

    dev: expose Android SDK emulator and adb in PATH

--- parts/dev/android.nix
+++ parts/dev/android.nix
@@ -1,11 +1,23 @@
-# Android Studio + SDK + émulateur
-{ pkgs, ... }:
-{
-  home-manager.users.vincent.home.packages = with pkgs; [
-    android-studio
-    jdk
-  ];
-
-  # KVM pour l'émulateur Android
-  users.users.vincent.extraGroups = [ "kvm" ];
-}
+# Android Studio + SDK + émulateur                                                                                                        
+  { pkgs, ... }:                                                                                                                            
+  {                                                                                                                                         
+    home-manager.users.vincent.home.packages = with pkgs; [                                                                                 
+      android-studio                                                                                                                        
+      jdk                                                                                                                                   
+    ];                                                                                                                                      
+                                                                                                                                          
+    # SDK géré par Android Studio dans ~/Android/Sdk                                                                                        
+    home-manager.users.vincent.home.sessionVariables = {                                                                                  
+      ANDROID_HOME = "$HOME/Android/Sdk";                                                                                                   
+      ANDROID_SDK_ROOT = "$HOME/Android/Sdk";
+    };                                                                                                                                      
+                                                                                                                                          
+    # Binaires emulator + adb dans le PATH                                                                                                  
+    home-manager.users.vincent.home.sessionPath = [
+      "$HOME/Android/Sdk/emulator"                                                                                                          
+      "$HOME/Android/Sdk/platform-tools"                                                                                                  
+    ];                                                                                                                                      
+   
+    # KVM pour l'émulateur Android                                                                                                          
+    users.users.vincent.extraGroups = [ "kvm" ];                                                                                          
+  }
